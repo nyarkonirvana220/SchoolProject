5a5cd2bc1804fc1b8603388c6f72462ef9bef1a9
nyarkonirvana220 <nyarkonirvana220@gmail.com>
2025-03-15 13:07:34 +0000

    Add 11.js

---
+++ src/11.js
@@ -0,0 +1,17 @@
+
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
